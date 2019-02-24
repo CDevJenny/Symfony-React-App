38413c626186450d598cb5e892a798ea013b2ae1
Cdevjenny <clement.jenny@hotmail.fr>
2019-02-24 19:22:48 +0100

    SF API + React APP

---
+++ api/src/Controller/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends Controller
+{
+
+}
